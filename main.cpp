435e4c72457421b59922da9e3f24f3e266a1f1d7
Guy Rutenberg <guyrutenberg@gmail.com>
2014-01-31 19:04:45 +0200

    Add support for interpreted programs.

--- main.cpp
+++ main.cpp
@@ -93,30 +93,58 @@ string get_parent_process_path()
 	return parent_path;
 }
 
-string get_key(string parent_path)
+/**
+ * Get the parent process' command line.
+ */
+vector<string> get_parent_process_cmdline()
 {
-	byte secret_key[KEY_SIZE]; // this should be replaced by something that will zero out the memory once destructed.
+	pid_t ppid = getppid();
+	boost::format proc_path("/proc/%d/cmdline");
+	fstream cmdline_file(boost::str(proc_path % ppid), ios::binary | ios::in);
+	vector<string> arguments;
+
+	for (string argument; getline(cmdline_file, argument, '\0');) {
+		arguments.push_back(argument);
+	}
+
+	return arguments;
+}
+
+
+/**
+ * Returns the secret key from the configuration file.
+ */
+vector<byte> get_secret_key()
+{
+	vector<byte> secret_key(KEY_SIZE);
 	fstream sec_file(SECRET_FILE, ios::binary | ios::in);
 	if (!sec_file) {
 		exit(IO_ERROR);
 	}
-	sec_file.read(reinterpret_cast<char *>(secret_key), sizeof(secret_key));
-	if (!sec_file.good() || sec_file.gcount() != sizeof(secret_key)) {
+	sec_file.read((char *) secret_key.data(), secret_key.size());
+	if (!sec_file.good() || sec_file.gcount() != secret_key.size()) {
 		exit(IO_ERROR);
 	}
+	return secret_key;
+}
+
+
+string get_key(string parent_path)
+{
+	vector<byte> secret_key = get_secret_key();
 	uid_t user = getuid();
 	string salt = (std::to_string(user) +
-		string("\x00", 1) + 
+		string("\x00", 1) +
 		parent_path);
 	string final_key;
 
 	try {
-		HMAC< SHA256 > hmac(secret_key, sizeof(secret_key));
+		HMAC< SHA256 > hmac(secret_key.data(), secret_key.size());
 
-		StringSource(salt, true, 
+		StringSource(salt, true,
 				new HashFilter(hmac,
 					new StringSink(final_key)
-					) // HashFilter      
+					) // HashFilter
 			    ); // StringSource
 	} catch(const CryptoPP::Exception& e) {
 		cerr << e.what() << endl;
@@ -126,22 +154,150 @@ string get_key(string parent_path)
 	return final_key;
 }
 
+/**
+ * Get the derived key for an interpreted program.
+ */
+string get_key_interpreted(string interpreter, string script_name)
+{
+	vector<byte> secret_key = get_secret_key();
+	uid_t user = getuid();
+	string salt = (std::to_string(user) +
+		string("\x00", 1) +
+		interpreter +
+		string("\x00", 1) +
+		script_name);
+	string final_key;
+
+	try {
+		HMAC< SHA256 > hmac(secret_key.data(), secret_key.size());
+
+		StringSource(salt, true,
+				new HashFilter(hmac,
+					new StringSink(final_key)
+					) // HashFilter
+			    ); // StringSource
+	} catch(const CryptoPP::Exception& e) {
+		cerr << e.what() << endl;
+		exit(1);
+	}
+
+	return final_key;
+}
+
+string encrypt_stdin(string parent_path, string interpreter)
+{
+	string output;
+	string key;
+
+	if (interpreter.size()) {
+		key = get_key_interpreted(interpreter, parent_path);
+	} else {
+		key = get_key(parent_path);
+	}
+
+	AutoSeededRandomPool prng;
+	byte iv[ IV_SIZE ];
+	prng.GenerateBlock( iv, sizeof(iv) );
+	output = string(reinterpret_cast<char *>(iv), sizeof(iv));
+
+	GCM<AES>::Encryption e;
+	e.SetKeyWithIV((const byte*)key.data(), key.size(), iv, sizeof(iv));
+
+	FileSource( cin, true,
+		new AuthenticatedEncryptionFilter( e,
+			new StringSink( output )
+			) // AuthenticatedEncryptionFilter
+		); // StringSource
+	return output;
+}
+
+string decrypt_stdin(string interpreter)
+{
+	string output;
+	string key;
+
+	if (interpreter.size()) {
+		auto cmd_line = get_parent_process_cmdline();
+		if (cmd_line.size() < 2) {
+			exit(INVALID_INPUT);
+		}
+
+		string script_name = cmd_line[1];
+		key = get_key_interpreted(interpreter, script_name);
+	} else {
+		key = get_key(get_parent_process_path());
+	}
+
+	byte iv[ AES::BLOCKSIZE ];
+	cin.read(reinterpret_cast<char *>(iv), sizeof(iv));
+	if (!cin) {
+		exit(INVALID_INPUT);
+	}
+	GCM< AES >::Decryption d;
+	d.SetKeyWithIV( (const byte*)key.data(), key.size(), iv, sizeof(iv) );
+
+	try {
+		AuthenticatedDecryptionFilter df( d,
+				new StringSink( output ),
+				AuthenticatedDecryptionFilter::DEFAULT_FLAGS
+				); // AuthenticatedDecryptionFilter
+
+		// The StringSource dtor will be called immediately
+		//  after construction below. This will cause the
+		//  destruction of objects it owns. To stop the
+		//  behavior so we can get the decoding result from
+		//  the DecryptionFilter, we must use a redirector
+		//  or manually Put(...) into the filter without
+		//  using a StringSource.
+		FileSource ( cin, true,
+				new Redirector( df /*, PASS_EVERYTHING */ )
+			    ); // StringSource
+
+		// If the object does not throw, here's the only
+		//  opportunity to check the data's integrity
+		bool b = df.GetLastResult();
+		assert( true == b );
+	} catch( CryptoPP::HashVerificationFilter::HashVerificationFailed& e ) {
+		cerr << "Caught HashVerificationFailed..." << endl;
+		cerr << e.what() << endl;
+		cerr << endl;
+		exit(INVALID_INPUT);
+	} catch( CryptoPP::InvalidArgument& e ) {
+		cerr << "Caught InvalidArgument..." << endl;
+		cerr << e.what() << endl;
+		cerr << endl;
+		exit(INVALID_INPUT);
+	} catch( CryptoPP::Exception& e ) {
+		cerr << "Caught Exception..." << endl;
+		cerr << e.what() << endl;
+		cerr << endl;
+		exit(INVALID_INPUT);
+	}
+
+	return output;
+}
 
 int main(int argc, char **argv)
 {
-	string file_name;
+	string parent_path;
+	string interpreter;
 	po::options_description desc("Options");
         desc.add_options()
 		("help,h", "display this help message and exit")
 		("version", "output version information and exit")
-		("encrypt,e", po::value<string>(&file_name),
+		("encrypt,e", po::value<string>(&parent_path),
 			"encrypt the input for the specified program.")
+		("interpreter,i", po::value<string>(&interpreter)->implicit_value(""),
+			"When decrypting the input, assume that the process is "
+			"executed via interpreter. If encrypting data for a "
+			"interpreted program, use this option to specify which"
+			"interpreter is used.")
         ;
 
 	po::variables_map vm;
 	try {
 		po::store(po::parse_command_line(argc, argv, desc), vm);
-		po::notify(vm);    
+		po::notify(vm);
 	} catch ( const po::error& e ) {
 		cerr << PACKAGE ": " << e.what() << endl;
 		cerr << "Try `" PACKAGE " --help' for more information. "
@@ -163,68 +319,15 @@ int main(int argc, char **argv)
 
 	string output;
 	if (vm.count("encrypt")) {
-		string key = get_key(file_name);
-		AutoSeededRandomPool prng;
-		byte iv[ IV_SIZE ];
-		prng.GenerateBlock( iv, sizeof(iv) );    
-		output = string(reinterpret_cast<char *>(iv), sizeof(iv));
-
-		GCM<AES>::Encryption e;
-		e.SetKeyWithIV((const byte*)key.data(), key.size(), iv, sizeof(iv));
-
-		FileSource( cin, true,
-			new AuthenticatedEncryptionFilter( e,
-				new StringSink( output )
-				) // AuthenticatedEncryptionFilter
-			); // StringSource
+		if (vm.count("interpreter") && interpreter.size() == 0) {
+			interpreter = get_parent_process_path();
+		}
+		output = encrypt_stdin(parent_path, interpreter);
 	} else { // decrypt whatever on stdin
-		string key = get_key(get_parent_process_path());
-		byte iv[ AES::BLOCKSIZE ];
-		cin.read(reinterpret_cast<char *>(iv), sizeof(iv));
-		if (!cin) {
-			// couldn't 
-			exit(INVALID_INPUT);
+		if (vm.count("interpreter") && interpreter.size() == 0) {
+			interpreter = get_parent_process_path();
 		}
-		GCM< AES >::Decryption d;
-		d.SetKeyWithIV( (const byte*)key.data(), key.size(), iv, sizeof(iv) );
-
-		try {
-			AuthenticatedDecryptionFilter df( d,
-					new StringSink( output ),
-					AuthenticatedDecryptionFilter::DEFAULT_FLAGS
-					); // AuthenticatedDecryptionFilter
-		
-			// The StringSource dtor will be called immediately
-			//  after construction below. This will cause the
-			//  destruction of objects it owns. To stop the
-			//  behavior so we can get the decoding result from
-			//  the DecryptionFilter, we must use a redirector
-			//  or manually Put(...) into the filter without
-			//  using a StringSource.
-			FileSource ( cin, true,
-					new Redirector( df /*, PASS_EVERYTHING */ )
-				    ); // StringSource
-		
-			// If the object does not throw, here's the only
-			//  opportunity to check the data's integrity
-			bool b = df.GetLastResult();
-			assert( true == b );
-		} catch( CryptoPP::HashVerificationFilter::HashVerificationFailed& e ) {
-			cerr << "Caught HashVerificationFailed..." << endl;
-			cerr << e.what() << endl;
-			cerr << endl;
-			exit(INVALID_INPUT);
-		} catch( CryptoPP::InvalidArgument& e ) {
-			cerr << "Caught InvalidArgument..." << endl;
-			cerr << e.what() << endl;
-			cerr << endl;
-			exit(INVALID_INPUT);
-		} catch( CryptoPP::Exception& e ) {
-			cerr << "Caught Exception..." << endl;
-			cerr << e.what() << endl;
-			cerr << endl;
-			exit(INVALID_INPUT);
-		}
+		output = decrypt_stdin(interpreter);
 	}
 
 	cout << output ;
